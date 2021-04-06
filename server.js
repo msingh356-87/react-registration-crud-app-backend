e564f075fa4220f4e60a3fb31df1d37cef965106
Madhurendra Singh <madhurendra.s@kairostech.com>
2021-04-06 10:02:08 +0530

    Test

--- server.js
+++ server.js
@@ -47,11 +47,7 @@ app.listen(port, () => console.log("Hello"))
 
 
 app.use(function(req, res, next) {
-    /*var err = new Error('Not Found');
-     err.status = 404;
-     next(err);*/
 
-    // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
@@ -60,8 +56,5 @@ app.use(function(req, res, next) {
     // Request headers you wish to allow
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
 
-    //  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-    // Pass to next layer of middleware
     next();
 });
